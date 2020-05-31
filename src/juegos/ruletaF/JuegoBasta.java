3dbbedc02c9065f3b581010d005a84be5d825ba4
Losoyaq0 <luis.losoya@uabc.edu.mx>
2020-05-31 03:47:53 -0700

    actualizacion basta

--- src/juegos/ruletaF/JuegoBasta.java
+++ src/juegos/ruletaF/JuegoBasta.java
@@ -257,17 +257,6 @@ public class JuegoBasta extends JFrame implements Runnable{
 
         Reiniciar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
-               /* valor=valor2=0;
-                cajaTexto.setText("");
-                cajaTexto.setEnabled(false);
-                label.setIcon(labelImageP);
-                label.repaint();
-                label.validate();
-                label2.setIcon(labelImage2P);
-                label2.repaint();
-                label2.validate();
-
-            pararCronometro();*/
                 reiniciarTodo();
             }
         });
@@ -357,26 +346,19 @@ public class JuegoBasta extends JFrame implements Runnable{
 
     public void run(){
         Integer minutos = 0 , segundos = 0, milesimas = 0;
-        //min es minutos, seg es segundos y mil es milesimas de segundo
         String min="", seg="", mil="";
         try
         {
-            //Mientras cronometroActivo sea verdadero entonces seguira
-            //aumentando el tiempo
             while( cronometroActivo )
             {
                 Thread.sleep( 4 );
                 //Incrementamos 4 milesimas de segundo
                 milesimas += 4;
 
-                //Cuando llega a 1000 osea 1 segundo aumenta 1 segundo
-                //y las milesimas de segundo de nuevo a 0
                 if( milesimas == 1000 )
                 {
                     milesimas = 0;
                     segundos += 1;
-                    //Si los segundos llegan a 60 entonces aumenta 1 los minutos
-                    //y los segundos vuelven a 0
                     if( segundos == 60 )
                     {
                         segundos = 0;
@@ -384,8 +366,7 @@ public class JuegoBasta extends JFrame implements Runnable{
                     }
                 }
 
-                //Esto solamente es estetica para que siempre este en formato
-                //00:00:000
+
                 if( minutos < 10 ) min = "0" + minutos;
                 else min = minutos.toString();
                 if( segundos < 10 ) seg = "0" + segundos;
@@ -395,7 +376,6 @@ public class JuegoBasta extends JFrame implements Runnable{
                 else if( milesimas < 100 ) mil = "0" + milesimas;
                 else mil = milesimas.toString();
 
-                //Colocamos en la etiqueta la informacion
                 tiempo.setText( min + ":" + seg + ":" + mil );
             }
         }catch(Exception e){}
