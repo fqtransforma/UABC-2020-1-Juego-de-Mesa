1567db26e04888195d31cc7aa66515f12b83d219
Brian <brian.soto44@uabc.edu.mx>
2020-06-12 10:10:18 -0700

    Creditos añadidos y musica bajada de volumen

--- src/juegos/mainmenu/MenuRun.java
+++ src/juegos/mainmenu/MenuRun.java
@@ -15,9 +15,7 @@ import general.Objeto2D;
 import general.Sonido;
 import general.Teclado;
 import juegos.ecotwister.Juego;
-import juegos.ruleta.EcoPregunta;
 import juegos.ruletaF.RuletaGui;
-import juegos.serpientesyescaleras.Game;
 import juegos.serpientesyescaleras.Ventana;
 
 import javax.swing.*;
@@ -35,7 +33,7 @@ public class MenuRun extends  JPanel implements Runnable{
     public static Sonido OST = new Sonido("src/resources/mainmenu_src/audio/musica/TownTheme.wav",true);
     public static Sonido selectSound = new Sonido("src/resources/mainmenu_src/audio/efectos/gmae.wav",false);
 
-    boolean sesion=false;
+    boolean creditos=false;
 
     private static volatile boolean onOff;
     private double fps = 0;
@@ -60,14 +58,6 @@ public class MenuRun extends  JPanel implements Runnable{
         frame.setVisible(true);
         frame.setTitle("Juego de Mesa -");
         OST.play();
-        /*
-        JOptionPane jp = new JOptionPane();
-        String name;
-        do{
-            name = jp.showInputDialog(frame, "¿Cual es un nombre?");
-        }while(name == null);
-        frame.setTitle("Juego de Mesa - Usuario:"+name);*/
-        sesion=true;
     }
 
     @Override
@@ -150,15 +140,17 @@ public class MenuRun extends  JPanel implements Runnable{
             }
             else if(coorX > 760 && coorX < 1020) {
                 selectSound.restart();
-                EcoPregunta.run();
+                RuletaGui.run();
                 OST.pause();
             }
             else if(coorX > 1170 && coorX < 1420) {
                 selectSound.restart();
-                RuletaGui.run();
-                OST.pause();
+                if(!creditos){
+                    creditos=true;
+                    JOptionPane.showMessageDialog(null,"Este proyecto fue posible gracias a la participación de: \n1 Bastida Gastelum Axel Fernando \n2 Gaytan Carballo Melissa \n3 Rodriguez Gonzalez Enola Carely \n4 Preciado Gudiño Guillermina Alejandra \n5 Cruz Terrazas Claudia Araceli \n6 Perez Luna Maria Del Carmen \n7 Rojero Martinez Jezreel Antonio \n8 Corrales Quintero Erick Roberto \n9 Cota Robledo Benjamin \n10 Rocha Castro Jacinto \n11 Crisantos Uscanga Luis Alberto \n12 Cruz Sanchez Miriam Fernanda \n13 Guerra Cervantes Sergio Enrique \n14 Soto Elenes Brian Ramiro \n15 Espinoza Andrade Angel Fernando \n16 Hong Lang Haolin \n17 Jimenez Bustillos Angel \n18 Vazquez Colin Antonio Guadalupe \n19 Losoya Gutierrez Luis Fernando \n20 Navarrete Solano Cristian Osbaldo\nEn conjunto con Fundacion Que Transforma");
+                }
             }
-            else if(coorX > 1570 && coorX < 1815){
+            else if(coorX > 1570 && coorX < 1815) {
                 selectSound.restart();
                 System.out.println("Aqui va lo de clasificacion");
                 OST.pause();
@@ -170,6 +162,7 @@ public class MenuRun extends  JPanel implements Runnable{
             }
         }
         teclado.enter = false;
+        creditos = false;
 
     }
     public synchronized void init() {
@@ -196,12 +189,8 @@ public class MenuRun extends  JPanel implements Runnable{
     public void paintComponent(Graphics g){
         this.repaint();
         actualizar();
-        if(sesion){
-            g.drawImage(fondo.getImage(), fondo.getX(), fondo.getY(), fondo.getSizex()*3, fondo.getSizey()*3, this);
-            g.drawImage(personaje.getImage(), personaje.getX(), personaje.getY(), this);
-        }
-        else
-            g.drawImage(fondo1.getImage(), fondo1.getX(), fondo1.getY(), fondo1.getSizex(), fondo1.getSizey(), this);
+        g.drawImage(fondo.getImage(), fondo.getX(), fondo.getY(), fondo.getSizex()*3, fondo.getSizey()*3, this);
+        g.drawImage(personaje.getImage(), personaje.getX(), personaje.getY(), this);
         setOpaque(false);
         super.paintComponent(g);
 
